eb3047e88fa5ef01ee536251e3142f9962ec7dd7
Jørn Sandvik Nilsson <joern.nilsson@gmail.com>
2016-10-17 16:29:40 +0200

    adds configurable port

--- server.js
+++ server.js
@@ -133,6 +133,6 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on: http://localhost:%s", process.env.PORT || 8080);
 });
 
